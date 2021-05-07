abef7c69f1db71f6786a6d283f8b0c996c01ff27
sathyasubramanitr <83812693+sathyasubramanitr@users.noreply.github.com>
2021-05-07 14:34:20 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,7 @@
+class Box extends Parent {
+  constructor(x, y, width, height) {
+    super(x, y, width, height)
+    this.image = loadImage('sprites/wood1.png')
+  }
+
+};
---
+++ Pig.js
@@ -0,0 +1,7 @@
+class Pig extends Parent{
+    constructor(x, y) {
+      super(x,y,50, 50);
+      this.image = ('sprites/enemy.png');
+    }
+  };
+  
